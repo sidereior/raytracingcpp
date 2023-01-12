b4ecbf051432172d457aed3e0db48578ea7affac
sidereior <alexanderlnanda@gmail.com>
2023-01-12 11:09:19 -0500

    fixed vector normals

--- main.cpp
+++ main.cpp
@@ -5,22 +5,42 @@
 #include <iostream>
 
 
-bool hit_sphere(const point3& center, double radius, const ray& r){
+double hit_sphere(const point3& center, double radius, const ray& r){
     vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(),r.direction());
+    auto a = dot(r.direction(), r.direction());
     auto b = 2.0 * dot(oc, r.direction());
     auto c = dot(oc,oc) - radius*radius;
     auto discriminant = b*b - 4*a*c;
-    //return discriminant > 0; just to see if 
-    return discriminant > 0;
+    if (discriminant < 0){
+        return -1.0;
+    } else {
+        return (-b - sqrt(discriminant)) / (2.0*a);
+    }
+    //return (discriminant > 0);
+    /*
+    if(discriminant < 0)
+    {
+        return -1.0;
+    }
+    else {
+        return (-b - sqrt(discriminant)) / (2.0*a);
+    }
+    */
 }
 
 color ray_color(const ray& r){
-    if(hit_sphere(point3(0,0,-1), 0.5, r))
-        return color(1,0,0);
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if(t > 0.0){
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
+    /*
+    if (hit_sphere(point3(0,0,-1), 0.5, r))
+        return color(1, 0, 0);
+    */
     vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-t)*color(1.0,1.0,1.0) + t*color(0.5,0.7,1.0);
+    t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
 
 int main() {
@@ -32,9 +52,8 @@ int main() {
     auto origin = point3(0,0,0);
     auto viewport_width = aspect_ratio * viewport_height;
     auto horizontal = vec3(viewport_width, 0, 0);
-    auto focal_length = 1.0;//what is the different between focal length and focal distance??
+    auto focal_length = 1.0;
     auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0,0,focal_length);
-   
     //rows written top to bottom
     //pixels in each row written left to right
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
